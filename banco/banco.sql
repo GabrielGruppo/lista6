35339f4b47fea157a153fba1809e17a2ed328a9e
GabrielGruppo <gabrielmgruppogg@gmail.com>
2023-06-06 08:53:55 -0300

    tst

---
+++ banco/banco.sql
@@ -0,0 +1,21 @@
+DROP  DATABASE IF EXISTS lista6;
+CREATE DATABASE lista6;
+
+USE lista6;
+
+CREATE TABLE agenda 
+(
+	id_agenda int not null auto_increment,
+	nome varchar(50) not null,
+	apelido varchar(40) not null,
+	endereco varchar(70) not null,
+	bairro varchar(50) not null,
+	cidade varchar(50) not null,
+	estado varchar(2) not null,
+	telefone varchar(15) not null,
+	celular varchar(15) not null,
+	email varchar(70) not null,
+	dt_cadastro date not null,
+	PRIMARY KEY(id_agenda)
+    
+);
